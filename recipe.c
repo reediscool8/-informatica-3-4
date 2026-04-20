6ea360431f4fe40adebe3c1a755e382b762b24b1
reediscool8 <reedthefarmer1@gmail.com>
2026-04-20 14:53:33 +0000

    recipe stuff

---
+++ recipe.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void){
+    printf("Coconut Cranberry Dessert\n");
+    printf("\n");
+    printf("1 1/2   cups graham cracker crumbs about 24 full-sized graham crackers\n");
+    
+}
