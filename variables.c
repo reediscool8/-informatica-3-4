3f1672ba678b471576761cf92ed8a66e3e678b83
reediscool8 <reedthefarmer1@gmail.com>
2026-04-21 14:57:25 +0000

    commit

---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// I added include
+int main(void){
+int number;
+// I deleted the !
+int test_value;
+// I removed the space
+int example;
+// I removed the number
+float int_value;
+// got rid of int and made it int_count
+printf("Variables created successfully.\n");
+}
