c742d73d2aba704e2a8259d79c3b0aacb4bbf860
reediscool8 <reedthefarmer1@gmail.com>
2026-04-21 15:48:16 +0000

    commit

---
+++ Vending.c
@@ -0,0 +1,38 @@
+#include <stdio,h>
+int main(void){
+    printf("Vending Machine Stock\n");
+    printf("------------------------------\n");
+    //ribeye
+    float price1 = 199.99;
+    int quantity1 = 8;
+    char select1 = 'R'
+//Ice cream
+    float price2 = 69.99;
+    int quantity2 = 13;
+    char select2 = 'I';
+    // blue cheese
+    float price3 = 29.99;
+    int quantity3 = 189;
+    char select3 = 'B'
+    //now i strat to print it
+    printf("Item name: Ribeye\n");
+    printf("Price: )
+
+}
+Vending Machine Stock
+---------------------------
+Item name: Chips
+Price: $1.50
+Quantity available: 20
+Selection code: A
+
+Item name: Soda
+Price: $2.00
+Quantity available: 15
+Selection code: B
+
+Item name: Candy
+Price: $0.75
+Quantity available: 30
+Selection code: C
+
