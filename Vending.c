f61f04eb3fc64f18cc314a02c26cd5c2d186d1fc
reediscool8 <reedthefarmer1@gmail.com>
2026-04-22 15:24:03 +0000

    clear

--- Vending.c
+++ Vending.c
@@ -1,38 +1,41 @@
-#include <stdio,h>
+#include <stdio.h>
 int main(void){
+
     printf("Vending Machine Stock\n");
     printf("------------------------------\n");
+
     //ribeye
     float price1 = 199.99;
     int quantity1 = 8;
-    char select1 = 'R'
-//Ice cream
+    char select1 = 'R';
+
+    //Ice cream
     float price2 = 69.99;
     int quantity2 = 13;
     char select2 = 'I';
+
     // blue cheese
     float price3 = 29.99;
     int quantity3 = 189;
-    char select3 = 'B'
+    char select3 = 'B';
+
     //now i strat to print it
     printf("Item name: Ribeye\n");
-    printf("Price: )
+    printf("Price: = $%.2f\n",price1);
+    printf("Quantity Avalible: = %d\n",quantity1);
+    printf("Selection Code: = %c\n",select1);
 
+    printf("\n");
+
+    printf("Item name: ice cream\n");
+    printf("Price: = $%.2f\n",price2);
+    printf("Quantity Avalible: = %d\n",quantity2);
+    printf("Selection Code: = %c\n",select2);
+
+    printf("\n");
+
+     printf("Item name: blue cheese\n");
+    printf("Price: = $%.2f\n",price3);
+    printf("Quantity Avalible: = %d\n",quantity3);
+    printf("Selection Code: = %c\n",select3);
 }
-Vending Machine Stock
----------------------------
-Item name: Chips
-Price: $1.50
-Quantity available: 20
-Selection code: A
-
-Item name: Soda
-Price: $2.00
-Quantity available: 15
-Selection code: B
-
-Item name: Candy
-Price: $0.75
-Quantity available: 30
-Selection code: C
-
