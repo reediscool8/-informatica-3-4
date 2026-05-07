ee19f7e258cccb372b4f58e19f9eb21efaa24357
reediscool8 <reedthefarmer1@gmail.com>
2026-05-07 20:14:47 +0000

    commit

---
+++ tempreture2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void)
+{
+float tempreture, Fahrenheit;
+printf("Enter Tempreture in C: ");
+scanf("%f", &tempreture);
+
+
+// Calculations
+Fahrenheit = (tempreture / 1.8) + 32;//convertion
+
+// Results
+printf("°C %.2f = %.2f\n", tempreture, Fahrenheit);
+//-40 C = -40 F
+if(tempreture < 0){
+    printf("❄️ Freezing weather\n");//If not below 0 then put 🥶 Very cold weather, and so on
+}
+else if(tempreture < 10){
+    printf("🥶 Very cold weather\n");
+}
+else if(tempreture < 20){
+    printf("🧥 Chilly weather\n");
+}
+else if(tempreture < 30){
+    printf("🖼️ Normal weather\n");
+}
+else if(tempreture < 40){
+    printf("☀️ Hot weather\n");
+}
+else if(tempreture > 40){
+    printf("🔥 Very hot weather\n");
+}
+}
